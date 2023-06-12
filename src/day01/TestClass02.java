4e2c37bf6174c218b7f10d0fd7dfe48e4f3fb93d
seohee <test@test.123>
2023-06-12 15:31:34 +0900

    testclass02 생성

---
+++ src/day01/TestClass02.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class TestClass02 {
+	public static void main(String[] args) {
+		/*
+		 	escape
+		 	 - 문자열 안에서 특수한 기능을 한다.
+		 	 \n : 엔터값과 비슷하다. 
+		 	 \t : tab크기만큼 오른쪽으로 커서 이동
+		 	 \" : "(큰 따옴표)하나 표현
+		 	 \\ : 역슬러시 하나 표현
+		 */
+		
+		System.out.println("안\n녕\n하\n세요");
+		System.out.println("1\t234567\t12345\t1");
+		System.out.println("이름\t나이");
+		System.out.println("홍길동\t20살");
+		
+		System.out.println(" \" ");
+		System.out.println("c:\\0.핀테크\\내가 만든 폴더");
+		
+		
+	}
+}
